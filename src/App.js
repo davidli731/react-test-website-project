7841032f9181462b66edc59cf436c51275af96ac
David Li <davidli731@gmail.com>
2023-07-23 19:30:47 +1000

    clean up css and js code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
     return (
@@ -14,6 +17,9 @@ function App() {
                     <Navbar />
                     <Routes>
                         <Route path="/" exact element={<Home />} />
+                        <Route path="/products" element={<Products />} />
+                        <Route path="/services" element={<Services />} />
+                        <Route path="/signup" element={<SignUp />} />
                     </Routes>
                 </Router>
             </>
