c63210afebebc5a5617f4e1dc4e7b85f9eebbf6b
David Li <davidli731@gmail.com>
2023-07-24 16:23:44 +1000

    clean up code

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ import Products from "./components/pages/Products";
 import Services from "./components/pages/Services";
 import SignUp from "./components/pages/SignUp";
 
+function Paths() {
+    return (
+        <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/signup" element={<SignUp />} />
+        </Routes>
+    )
+}
+
 function App() {
     return (
         <div className="App">
             <>
                 <Router>
                     <Navbar />
-                    <Routes>
-                        <Route path="/" exact element={<Home />} />
-                        <Route path="/products" element={<Products />} />
-                        <Route path="/services" element={<Services />} />
-                        <Route path="/signup" element={<SignUp />} />
-                    </Routes>
+                    <Paths />
                 </Router>
             </>
         </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,55 +2,67 @@ import React from 'react'
 import "./Cards.css";
 import CardItem from "./CardItem";
 
+function DisplayCardsTop() {
+    return (
+        <ul className="cards_items">
+            <CardItem
+                src="/img-1.jpg"
+                alt="sunset"
+                text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                label="Adventure"
+                path="/services"
+            />
+            <CardItem
+                src="/img-2.jpg"
+                alt="beach"
+                text="Travel through the Islands of Bali in a Private Cruise"
+                label="Luxury"
+                path="/services"
+            />
+        </ul>
+    )
+}
+
+function DisplayCardsFollowing() {
+    return (
+        <ul className="cards_items">
+            <CardItem
+                src="/img-1.jpg"
+                alt="sunset"
+                text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                label="Adventure"
+                path="/services"
+            />
+            <CardItem
+                src="/img-2.jpg"
+                alt="beach"
+                text="Travel through the Islands of Bali in a Private Cruise"
+                label="Luxury"
+                path="/services"
+            />
+            <CardItem
+                src="/img-2.jpg"
+                alt="beach"
+                text="Travel through the Islands of Bali in a Private Cruise"
+                label="Luxury"
+                path="/services"
+            />
+        </ul>
+    )
+}
+
 function Cards() {
-  return (
-    <div className="cards">
-        <h1>Check out these EPIC Destinations!</h1>
-        <div className="cards_container">
-            <div className="cards_wrapper">
-                <ul className="cards_items">
-                    <CardItem 
-                        src="/img-1.jpg"
-                        alt="sunset"
-                        text="Explore the hidden waterfall deep inside the Amazon Jungle" 
-                        label="Adventure"
-                        path="/services" 
-                    />
-                    <CardItem 
-                        src="/img-2.jpg"
-                        alt="beach"
-                        text="Travel through the Islands of Bali in a Private Cruise" 
-                        label="Luxury"
-                        path="/services" 
-                    />
-                </ul>
-                <ul className="cards_items">
-                    <CardItem 
-                        src="/img-1.jpg"
-                        alt="sunset"
-                        text="Explore the hidden waterfall deep inside the Amazon Jungle" 
-                        label="Adventure"
-                        path="/services" 
-                    />
-                    <CardItem 
-                        src="/img-2.jpg"
-                        alt="beach"
-                        text="Travel through the Islands of Bali in a Private Cruise" 
-                        label="Luxury"
-                        path="/services" 
-                    />
-                    <CardItem 
-                        src="/img-2.jpg"
-                        alt="beach"
-                        text="Travel through the Islands of Bali in a Private Cruise" 
-                        label="Luxury"
-                        path="/services" 
-                    />
-                </ul>
+    return (
+        <div className="cards">
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className="cards_container">
+                <div className="cards_wrapper">
+                    <DisplayCardsTop />
+                    <DisplayCardsFollowing />
+                </div>
             </div>
-        </div>
-    </div>
-  )
+        </div >
+    )
 }
 
 export default Cards
